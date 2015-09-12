30c27d978ba273ab1f40e5583e2320cecf6f2067
dwkang1111 <dwkang1111@gmail.com>
2015-09-12 10:15:52 -0400

    font change, list skeleton

--- subliminal/subliminal.js
+++ subliminal/subliminal.js
@@ -14,19 +14,21 @@ if (Meteor.isClient) {
       event.preventDefault();
       // Get value from form element
       var text = event.target.tags.value;
-      // Insert a task into the collection
-      // Dreams.insert({
-      //   tags: text,
-      //   createdAt: new Date() // current time
-      // });
- 
-      // Clear form
-      event.target.tags.value = "";
+      if(text != "") {
+        // Insert a task into the collection
+        // Dreams.insert({
+        //   tags: text,
+        //   createdAt: new Date() // current time
+        // });
+   
+        // Clear form
+        event.target.tags.value = "";
 
-      $('#imageBar').height('0px');
-      $('#imageBar').animate({height: '+=200px'});
-      $('.imgBarimg').height('180px');
-      //$('.imgBarimg').animate({height: '+=180px'});
+        $('#imageBar').height('0px');
+        $('#imageBar').animate({height: '+=200px'});
+        $('.imgBarimg').height('180px');
+        //$('.imgBarimg').animate({height: '+=180px'});
+      }
     }
   });
 }
